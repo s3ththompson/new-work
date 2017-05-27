476e75a9135604262e7454b6cdec85eb07611483
Seth Thompson <s3th.thompson@gmail.com>
2017-05-26 18:36:17 -0700

    added more functionality

---
+++ views/default.js
@@ -0,0 +1,17 @@
+const bel = require('bel');
+
+module.exports = function(sites) {
+  return bel`<div>
+    <ul>
+    ${sites.map((site) => { return link(site) })}
+    </ul>
+  </div>`
+}
+
+function link(site) {
+  var newspan = site.new ? bel`<span class="new">New</span>` : '';
+  return bel`<li>
+    <a href="${site.url}">${site.name}</a>
+    ${newspan}
+  </li>`
+}
