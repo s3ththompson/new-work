4b9e7159cdd98997a78549564c89608108738cb4
Seth Thompson <s3th.thompson@gmail.com>
2017-05-26 18:44:17 -0700

    prettier

--- views/default.js
+++ views/default.js
@@ -3,15 +3,17 @@ const bel = require('bel');
 module.exports = function(sites) {
   return bel`<div>
     <ul>
-    ${sites.map((site) => { return link(site) })}
+    ${sites.map(site => {
+      return link(site);
+    })}
     </ul>
-  </div>`
-}
+  </div>`;
+};
 
 function link(site) {
   var newspan = site.new ? bel`<span class="new">New</span>` : '';
   return bel`<li>
     <a href="${site.url}">${site.name}</a>
     ${newspan}
-  </li>`
+  </li>`;
 }
