916ed1817fb02ad096e8d46438e59d4bf031092f
Seth Thompson <s3th.thompson@gmail.com>
2017-07-08 03:50:55 +0200

    fix default file locations in cli help text

--- bin/newwork.js
+++ bin/newwork.js
@@ -36,9 +36,9 @@ const helpText = `Usage: newwork <command> [options]
     ls                          List all sites in your new-work page
 
   Available options:
-    -i, --input <filename>      Input YAML file [default: sites.yaml]
-    -o, --output <filename>     Output HTML file [default: sites.html]
-    -l, --lockfile <filename>   Lockfile location [default: sites.lock]
+    -i, --input <filename>      Input YAML file [default: ~/.newwork.yaml]
+    -o, --output <filename>     Output HTML file [default: ./newwork.html]
+    -l, --lockfile <filename>   Lockfile location [default: ~/.newwork.lock]
     -p, --port=<n>              Bind 'newwork serve' to a port [default: 3030]
     -h, --help                  Print usage`;
 
